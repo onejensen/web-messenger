ede1b6cd0b41f5bfd3d70bae0a7a79532dd321e5
Onejensen <juanjocollgarcia@gmail.com>
2026-01-12 13:49:17 +0100

    Change from Render mail to Nodemailer

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,9 +1,7 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  secure: process.env.EMAIL_PORT == 465, // Use SSL for port 465
+  service: 'gmail', // This handles host, port, and security for Gmail automatically
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
